e18a23c8811f618e0d1f28fc2682d9ccc2ba3cd7
hiryleu0 <mikoajryll@gmail.com>
2020-10-26 13:19:35 +0100

    all done (less code)

--- Lab3-Components-master/src/Car.jsx
+++ Lab3-Components-master/src/Car.jsx
@@ -45,8 +45,6 @@ export default class Car extends React.Component
 
     render()
     {
-        if(this.state.editing)
-        {
             return(
             <table style={{border: "3px solid rgb(0,0,0)", margin: "2em", padding: "2em", width: "90%"}}>
                 <tbody>
@@ -62,43 +60,24 @@ export default class Car extends React.Component
                         <li>{this.state.doors} doors</li>
                         <li>air conditioning</li>
                     </td>
-                    <td>
-                        <p>Price per day</p>
-                        <input id="priceTextBox"/>
-                        <button onClick={this.onEditClick}>Save</button>
-                        <button onClick={this.onDeleteClick}>Delete</button>
-                    </td>
+                    {
+                        this.state.editing ? 
+                            <td>
+                                <p>Price per day</p>
+                                <input id="priceTextBox"/>
+                                <button onClick={this.onEditClick}>Save</button>
+                                <button onClick={this.onDeleteClick}>Delete</button>
+                            </td>
+                        :
+                            <td>
+                                <p>Price per day</p>
+                                <p style={{fontWeight: "bold", font: "15px"}}>{this.state.pricePerDay} PLN</p>
+                                <button onClick={this.onEditClick}>Edit</button>
+                                <button onClick={this.onDeleteClick}>Delete</button>
+                            </td>
+                    }
                 </tr>
                 </tbody>
             </table>)
         }
-        else
-        {
-            return(
-                <table style={{border: "3px solid rgb(0,0,0)", margin: "2em", padding: "2em", width: "90%"}}>
-                    <tbody>
-                    <tr>
-                        <td>
-                            <img height="150" src={this.state.image} alt=""/>
-                        </td>
-                        <td>
-                            <p style={{fontSize:"20px", fontWeight:"bold"}}>{this.state.name}</p>
-                        </td>
-                        <td>
-                            <li>{this.state.seats} seats</li>
-                            <li>{this.state.doors} doors</li>
-                            <li>air conditioning</li>
-                        </td>
-                        <td>
-                            <p>Price per day</p>
-                            <p style={{fontWeight: "bold", font: "15px"}}>{this.state.pricePerDay} PLN</p>
-                            <button onClick={this.onEditClick}>Edit</button>
-                            <button onClick={this.onDeleteClick}>Delete</button>
-                        </td>
-                    </tr>
-                    </tbody>
-                </table>
-            );
-        }
-    }
 }
